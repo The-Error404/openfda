77fe5f638389abff8715cb48c0cd1af6a369721c
José Berenguel Noguera <38032289+The-Error404@users.noreply.github.com>
2018-04-06 17:37:02 +0200

    Update Programa3.py

--- openfda-2/Programa3.py
+++ openfda-2/Programa3.py
@@ -1,8 +1,11 @@
 import requests
-r = requests.get('https://api.fda.gov/drug/label.json?search=active_ingredient:aspirin&limit=100&skip')
-main_dict = r.json()['results']
-man_list=[]
-for elem in main_dict:
-    if 'manufacturer_name' in elem['openfda']:
-        man_list.append(str(elem['openfda']["manufacturer_name"]).strip("['']"))
-print(set(man_list))
+try:  
+    r = requests.get('https://api.fda.gov/drug/label.json?search=active_ingredient:aspirin&limit=100&skip')
+    main_dict = r.json()['results']
+    man_list=[]
+    for elem in main_dict:
+        if 'manufacturer_name' in elem['openfda']:
+            man_list.append(str(elem['openfda']["manufacturer_name"]).strip("['']"))
+    print(set(man_list))
+except requests.exceptions.RequestException:
+    print("Connection refused (Check URL for mistypings)")
