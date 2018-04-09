60eb51a6dcb5eb2c7e3bf961a4ad5893c4016d95
José Berenguel Noguera <38032289+The-Error404@users.noreply.github.com>
2018-04-09 12:31:01 +0200

    Update Programa2.py

--- openfda-1/Programa2.py
+++ openfda-1/Programa2.py
@@ -2,6 +2,7 @@ import requests
 try:
     r = requests.get('https://api.fda.gov/drug/label.json?search=manufacturer_name:pfizer&limit=10')
     main_dict = r.json()['results']
+    print('IDs de 10 medicamentos:')
     for elem in main_dict:
         print(str(elem['openfda']['spl_id']).strip("['']"))
 except requests.exceptions.RequestException:
