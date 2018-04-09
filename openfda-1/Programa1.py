2b7a6649a2ad9fbad65a7508ff8e9c109ac2355c
José Berenguel Noguera <38032289+The-Error404@users.noreply.github.com>
2018-04-09 12:27:46 +0200

    Update Programa1.py

--- openfda-1/Programa1.py
+++ openfda-1/Programa1.py
@@ -5,8 +5,8 @@ try:
   id_info = str(main_dict['openfda']['spl_id']).strip("['']")
   man_info = str(main_dict['openfda']['manufacturer_name']).strip("['']")
   pur_info = str(main_dict['purpose']).strip("['']")
-  print(id_info)
-  print(man_info)
-  print(pur_info)
+  print("ID del producto:",id_info)
+  print("Información del fabricante:",man_info)
+  print("Usos:",pur_info)
   except requests.exceptions.RequestException:
     print("Connection refused (Check URL for mistypings)")
